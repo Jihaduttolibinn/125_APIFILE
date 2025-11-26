bf8e7929e024b04f422eb6e51283176cf7e4c5ae
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-11-26 10:50:51 +0700

    Menambahkan komik

---
+++ models/komik.js
@@ -0,0 +1,30 @@
+const { daysToWeeks } = require("date-fns")
+
+module.exports = (sequelize, DataTypes) => {
+    const Komik = sequelize.define('Komik', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            aoutoincrement: true
+        },
+        judul: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        penulis: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        dekskripsi: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        imageType : DataTypes.STRING,
+        imageName : DataTypes.STRING,
+        imageData : DataTypes.BLOB('long'),
+        }, 
+    {
+        tableName: 'komiks',
+    });
+    return Komik;
+};
