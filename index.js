53b765989198307f13fd7947761ef54f9a4f43fd
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-11-26 12:03:22 +0700

    SELESAI

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDatabase = require('./config/db');
-//const apiRoute = require('./routes/api');
+const apiRoute = require('./routes/api');
 
 const app = express();
 const PORT = 3000;
@@ -8,7 +8,8 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//app.use('/api/v1', apiRoute);
+// Mount API routes at /api
+app.use('/api', apiRoute);
 
 async function startServer() {
     await connectDatabase();
--- models/komik.js
+++ models/komik.js
@@ -1,30 +1,32 @@
-const { daysToWeeks } = require("date-fns")
-
 module.exports = (sequelize, DataTypes) => {
-    const Komik = sequelize.define('Komik', {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            aoutoincrement: true
+    const Komik = sequelize.define(
+        'Komik',
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            judul: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            penulis: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            deskripsi: {
+                type: DataTypes.TEXT,
+                allowNull: false,
+            },
+            imageType: DataTypes.STRING,
+            imageName: DataTypes.STRING,
+            imageData: DataTypes.BLOB('long'),
         },
-        judul: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        penulis: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        dekskripsi: {
-            type: DataTypes.TEXT,
-            allowNull: false
-        },
-        imageType : DataTypes.STRING,
-        imageName : DataTypes.STRING,
-        imageData : DataTypes.BLOB('long'),
-        }, 
-    {
-        tableName: 'Komik',
-    });
-    return Komik;
+        {
+            tableName: 'komiks',
+        }
+    );
+
+    return Komik;
 };
